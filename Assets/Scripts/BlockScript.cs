1671d85db972eb2620760d0e2dfa6492bbbcbdfa
Lily Rose (@lilykiwi) <lilykiwi9911@pm.me>
2022-04-18 16:06:35 +0100

    initial commit

---
+++ Assets/Scripts/BlockScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseOver() //Checks if mouse is over the object
+    {
+        if (Input.GetMouseButtonDown(0)) //Checks if mouse button 1 is pressed
+        {
+            //Code could be put here to spawn another object for mud or whatever
+
+            UnityEngine.Object p__n = this.gameObject;
+            Destroy(p__n); //Destroys the square
+        }
+    }
+}

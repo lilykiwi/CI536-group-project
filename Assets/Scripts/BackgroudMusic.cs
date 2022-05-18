e5a298d12e49d9ad77bea4481efebdd02a3c3cb5
CharlieKanatli <103531511+CharlieKanatli@users.noreply.github.com>
2022-05-18 16:51:25 +0100

    background music fixed

--- Assets/Scripts/BackgroudMusic.cs
+++ Assets/Scripts/BackgroudMusic.cs
@@ -5,16 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class BackgroudMusic : MonoBehaviour
 {
-
+    private AudioSource music;
+    private GameObject[] backgroundMusic;
     // Start is called before the first frame update
     void Start()
     {
-        DontDestroyOnLoad(this.gameObject);
+        backgroundMusic = GameObject.FindGameObjectsWithTag("BackgroundMusic");
+        if (backgroundMusic.Length != 0){
+            music = this.GetComponent<AudioSource>();
+            music.Play();
+            DontDestroyOnLoad(this.gameObject);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
